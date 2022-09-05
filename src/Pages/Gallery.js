2163f391a57a3796f1b4eca1808cca4aceabe0f3
Koladele Olaitan <koladeleolaitan@gmail.com>
2022-09-05 20:07:14 +0000

    Added gallery page.

--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -1,1 +1,9 @@
-export default function Gallery() {}
+export default function Gallery() {
+  return (
+    <div className="w-full mx-auto md:w-4/5 px-[24px] md:px-0 py-[24px]">
+      <h1 className="pb-2 mb-4 text-5xl font-normal border-b-4 md:font-bold text-primary border-primary">
+        Gallery
+      </h1>
+    </div>
+  );
+}
